450c97a08f39d3b261e0d5566db05acc61ac9c4a
felipe.ssantos77 <felipe.soares007@hotmail.com>
2022-09-22 21:24:52 -0300

    Tela cadastro

---
+++ app/src/main/java/com/example/charliebookstore/Activity_Cadastrar.kt
@@ -0,0 +1,11 @@
+package com.example.charliebookstore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_Cadastrar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastrar)
+    }
+}
