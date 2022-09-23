c50d871779bd049c08c0337c4cbe554f9ad571ba
Alexandre Fidalgo <alexandre.fidalgo@spread.com.br>
2022-09-23 17:38:55 -0300

    Margin

--- app/src/main/java/com/example/charliebookstore/activities/ActivityCadastrar.kt
+++ app/src/main/java/com/example/charliebookstore/activities/ActivityCadastrar.kt
@@ -1,9 +1,10 @@
-package com.example.charliebookstore
+package com.example.charliebookstore.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.charliebookstore.R
 
-class Activity_Cadastrar : AppCompatActivity() {
+class ActivityCadastrar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastrar)
