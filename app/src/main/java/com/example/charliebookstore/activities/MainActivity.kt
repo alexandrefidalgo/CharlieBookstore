770c3f481002b4cb12308e9c5bf41cde4726f6dd
Alexandre Fidalgo <alexandre.fidalgo@spread.com.br>
2022-09-24 10:09:04 -0300

    Click cadastro

--- app/src/main/java/com/example/charliebookstore/activities/MainActivity.kt
+++ app/src/main/java/com/example/charliebookstore/activities/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.charliebookstore.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.charliebookstore.R
 
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun startActivityCadastrar(view: View?) {
+        val ActivityCadastrar = Intent(this, ActivityCadastrar::class.java)
+        startActivity(ActivityCadastrar)
+    }
+
 }
