c00acf28aa846f27a339e6430eda20f098a943a0
Felipe Soares <felipe.soares007@hotmail.com>
2022-09-26 00:06:29 -0300

    Configuração Tela de Cadastro  - 26-09-2022 - Felps Iluminado

---
+++ app/src/main/java/com/example/charliebookstore/activities/FormCadastrar.kt
@@ -0,0 +1,12 @@
+package com.example.charliebookstore.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.charliebookstore.R
+
+class FormCadastrar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_cadastrar)
+    }
+}
