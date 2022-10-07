d1f6b0db2933c1aa3f9aa728e5a1ef4d598d5191
felipe.ssantos77 <felipe.soares007@hotmail.com>
2022-10-06 21:54:52 -0300

    AULA 06-10-2022 - Tela Inicial

--- app/src/main/java/com/example/charliebookstore/fragments/GruposFragment.kt
+++ app/src/main/java/com/example/charliebookstore/fragments/GruposFragment.kt
@@ -16,7 +16,7 @@ class GruposFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentGruposBinding.inflate(inflater)
 
-        binding.btListaFragment.setOnClickListener {
+        binding.btLista.setOnClickListener {
             //val intent = Intent(context, ListaProdutosFragment::class.java)
              //startActivity(intent)
 
