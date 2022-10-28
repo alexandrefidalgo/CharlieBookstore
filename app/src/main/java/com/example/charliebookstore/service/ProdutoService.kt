d567d9fd97b782ad5eec252f09126d008cfdc87b
alexandrefidalgo <alexandrefidalgo@hotmail.com>
2022-10-27 22:01:14 -0300

    Api2

--- app/src/main/java/com/example/charliebookstore/service/ProdutoService.kt
+++ app/src/main/java/com/example/charliebookstore/service/ProdutoService.kt
@@ -8,33 +8,33 @@ import retrofit2.http.*
 interface ProdutoService {
 
     //USADO NO PROJETO
-    @GET("/produtos")
+    @GET("/produto")
     fun listar(): Call<List<Produto>>
 
     //OUTROS EXEMPLOS
     //Parâmetro de URL (ex: http://endereco.com.br/endpoint/valorId)
-    @GET("/produtos/{id}")
+    @GET("/produto/{id}")
     fun get(@Path("id") id: Int): Call<Produto>
 
     //Outro exemplo de parâmetro de URL (ex: http://endereco.com.br/endpoint/valorNome)
-    @GET("/android/rest/produto/{nome}")
+    @GET("/produto/{nome}")
     fun pesquisar(@Path("nome") nome: String): Call<List<Produto>>
 
     //Outro exemplo de parâmetro de URL (ex: http://endereco.com.br/endpoint?nome=valorNome)
-    @GET("/android/rest/produto")
+    @GET("/produto")
     fun pesquisar2(@Query("nome") nome: String): Call<List<Produto>>
 
     //Outro exemplo de parâmetro de corpo de requisição, normalmente utilizado
     //no envio por requisições de inserção
-    @POST("/android/rest/produto")
+    @POST("/produto")
     fun inserir(@Body produto: Produto): Call<Produto>
 
     //Outro exemplo de parâmetro de corpo de requisição, normalmente utilizado
     //no envio por requisições de deleção
-    @PUT("/android/rest/produto")
+    @PUT("/produto")
     fun atualizar(@Body produto: Produto, @Query("id") id: Int): Call<Produto>
 
     //Requisição de deleção
-    @DELETE("/android/rest/produto")
+    @DELETE("/produto")
     fun excluir(@Query("id") id: Int): Call<Produto>
 }
